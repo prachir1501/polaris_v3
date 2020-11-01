5f539a82fd1d37e0ec6cf8ce277bee4e3070aa21
animeshraj123 <65447444+animeshraj123@users.noreply.github.com>
2020-11-01 10:41:55 +0530

    smp first commit

---
+++ mentors2k18.php
@@ -0,0 +1,388 @@
+<!DOCTYPE html>
+<html lang="en">
+
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <meta name="theme-color" content="#3498db">
+
+        <link href="https://fonts.googleapis.com/css?family=Lato:100,300,400,700,900|Bitter:700|Dancing+Script:700" rel="stylesheet">
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.13/css/all.css" integrity="sha384-DNOHZ68U8hZfKXOrtjWvjxusGo9WQnrNx2sqG0tfsghAvtVlRW3tvkXWZh58N9jp" crossorigin="anonymous">
+
+        <link rel="stylesheet" href="css/animate.css">
+        <link rel="stylesheet" href="css/style_v3.css">
+
+        <title>Polaris - Student Mentorship Program</title>
+
+
+        <!-- Global site tag (gtag.js) - Google Analytics -->
+		<script async src="https://www.googletagmanager.com/gtag/js?id=UA-121551593-1"></script>
+		<script>
+		  window.dataLayer = window.dataLayer || [];
+		  function gtag(){dataLayer.push(arguments);}
+		  gtag('js', new Date());
+
+		  gtag('config', 'UA-121551593-1');
+		</script>
+
+    </head>
+
+    <body>
+
+      <?php
+      require_once('header.php');
+      ?>
+      <!-- created by Animesh Raj 2019 batch -->
+      
+ <section class="section">
+            <div class="container">
+                <h2 class="heading-secondary">Mentor Coordinators</h2>
+            </div>
+            <div class="row">
+                <div class="dev">
+                    <div class="col-1-of-2">
+                        <figure class="dev__shape">
+                            <img src="img/2k18/mentor/coordinator1.jpg" alt="Pramit Bhattacharyya" class="dev__img">
+                            <figcaption class="dev__caption">Pramit Bhattacharyya</figcaption>
+                        </figure>
+                        <div class="dev__name"><strong>Pramit Bhattacharyya</strong></div>
+                        <a href="mailto:pramitb@iitbhilai.ac.in" class="dev__profile">
+                            <i class="far fa-envelope dev__profile--icon"></i>
+                        </a>
+                    </div>
+
+                    <div class="col-1-of-2">
+                        <figure class="dev__shape">
+                            <img src="img/2k18/mentor/coordinator2.jpg" alt="Yash Tiwari" class="dev__img">
+                            <figcaption class="dev__caption">Yash Tiwari</figcaption>
+                        </figure>
+                        <div class="dev__name"><strong>Yash Tiwari</strong></div>
+                        <a href="mailto:yashtiwari@iitbhilai.ac.in" class="dev__profile">
+                            <i class="far fa-envelope dev__profile--icon"></i>
+                        </a>
+                    </div>
+                    </div>
+            </div>
+        </section>
+
+        <div class="line-break"></div>
+        
+        <section class="section">
+            <div class="container">
+                <h2 class="heading-secondary">Mentors-UG</h2>
+            </div>
+
+            <!-- EDIT START HERE -->
+
+            <div class="row">
+                <div class="dev">
+                    <div class="col-1-of-4">
+                        <figure class="dev__shape">
+                            <img src="img/2k18/mentor/mentor1.jpeg" alt="Aryan Jain" class="dev__img">
+                            <figcaption class="dev__caption">Aryan Jain</figcaption>
+                        </figure>
+                        <div class="dev__name"><strong>Aryan Jain</strong></div>
+                        <a href="mailto:aryanj@iitbhilai.ac.in" class="dev__profile">
+                            <i class="far fa-envelope dev__profile--icon"></i>
+                        </a>
+                    </div>
+
+                    <div class="col-1-of-4">
+                        <figure class="dev__shape">
+                            <img src="img/2k18/mentor/mentor2.jpg" alt="Ashutosh Sahu" class="dev__img">
+                            <figcaption class="dev__caption">Ashutosh Sahu</figcaption>
+                        </figure>
+                        <div class="dev__name"><strong>Ashutosh Sahu</strong></div>
+                        <a href="mailto:ashutoshsahu@iitbhilai.ac.in" class="dev__profile">
+                            <i class="far fa-envelope dev__profile--icon"></i>
+                        </a>
+                    </div>
+
+                    <div class="col-1-of-4">
+                        <figure class="dev__shape">
+                            <img src="img/2k18/mentor/mentor20.jpeg" alt="Ashutosh Soni" class="dev__img">
+                            <figcaption class="dev__caption">Ashutosh Soni</figcaption>
+                        </figure>
+                        <div class="dev__name"><strong>Ashutosh Soni</strong></div>
+                        <a href="mailto:ashutoshs@iitbhilai.ac.in" class="dev__profile">
+                            <i class="far fa-envelope dev__profile--icon"></i>
+                        </a>
+                    </div>
+
+                    <div class="col-1-of-4">
+                        <figure class="dev__shape">
+                            <img src="img/2k18/mentor/mentor3.jpg" alt="Divyansh Khandelwal" class="dev__img">
+                            <figcaption class="dev__caption">Divyansh Khandelwal</figcaption>
+                        </figure>
+                        <div class="dev__name"><strong>Divyansh Khandelwal</strong></div>
+                        <a href="mailto:divyanshk@iitbhilai.ac.in" class="dev__profile">
+                            <i class="far fa-envelope dev__profile--icon"></i>
+                        </a>
+                    </div>
+
+
+                </div>
+            </div>
+
+            <div class="row">
+                <div class="dev">
+                  <div class="col-1-of-4">
+                      <figure class="dev__shape">
+                          <img src="img/2k18/mentor/mentor4.jpg" alt="Driti Singh" class="dev__img">
+                          <figcaption class="dev__caption">Driti Singh</figcaption>
+                      </figure>
+                      <div class="dev__name"><strong>Driti Singh</strong></div>
+                      <a href="mailto:dritis@iitbhilai.ac.in" class="dev__profile">
+                          <i class="far fa-envelope dev__profile--icon"></i>
+                      </a>
+                  </div>
+
+                    <div class="col-1-of-4">
+                        <figure class="dev__shape">
+                            <img src="img/2k18/mentor/mentor5.jpg" alt="Gundu Shreya" class="dev__img">
+                            <figcaption class="dev__caption">Gundu Shreya</figcaption>
+                        </figure>
+                        <div class="dev__name"><strong>Gundu Shreya</strong></div>
+                        <a href="mailto:gundus@iitbhilai.ac.in" class="dev__profile">
+                            <i class="far fa-envelope dev__profile--icon"></i>
+                        </a>
+                    </div>
+
+                    <div class="col-1-of-4">
+                        <figure class="dev__shape">
+                            <img src="img/2k18/mentor/mentor6.jpg" alt="Guntuku Sai Rishitha" class="dev__img">
+                            <figcaption class="dev__caption">Guntuku Sai Rishitha</figcaption>
+                        </figure>
+                        <div class="dev__name"><strong>Guntuku Sai Rishitha</strong></div>
+                        <a href="mailto:guntukur@iitbhilai.ac.in" class="dev__profile">
+                            <i class="far fa-envelope dev__profile--icon"></i>
+                        </a>
+                    </div>
+
+                    <div class="col-1-of-4">
+                        <figure class="dev__shape">
+                            <img src="img/2k18/mentor/mentor7.jpg" alt="Himani Madaan" class="dev__img">
+                            <figcaption class="dev__caption">Himani Madaan</figcaption>
+                        </figure>
+                        <div class="dev__name"><strong>Himani Madaan</strong></div>
+                        <a href="mailto:himanim@gmail.com" class="dev__profile">
+                            <i class="far fa-envelope dev__profile--icon"></i>
+                        </a>
+                    </div>
+
+                </div>
+            </div>
+
+            <div class="row">
+                <div class="dev">
+
+                  <div class="col-1-of-4">
+                      <figure class="dev__shape">
+                          <img src="img/2k18/mentor/mentor8.jpg" alt="Ketha Jaya Sandeep" class="dev__img">
+                          <figcaption class="dev__caption">Ketha Jaya Sandeep</figcaption>
+                      </figure>
+                      <div class="dev__name"><strong>Ketha Jaya Sandeep</strong></div>
+                      <a href="mailto:kethas@iitbhilai.ac.in" class="dev__profile">
+                          <i class="far fa-envelope dev__profile--icon"></i>
+                      </a>
+                  </div>
+
+                    <div class="col-1-of-4">
+                        <figure class="dev__shape">
+                            <img src="img/2k18/mentor/mentor9.jpg" alt="Kolli Venkata Madhukar" class="dev__img">
+                            <figcaption class="dev__caption">Kolli Venkata Madhukar</figcaption>
+                        </figure>
+                        <div class="dev__name"><strong>Kolli Venkata Madhukar</strong></div>
+                        <a href="mailto:kollim@iitbhilai.ac.in" class="dev__profile">
+                            <i class="far fa-envelope dev__profile--icon"></i>
+                        </a>
+                    </div>
+
+                    <div class="col-1-of-4">
+                        <figure class="dev__shape">
+                            <img src="img/2k18/mentor/mentor10.jpg" alt="Nikhil Kumar Suman" class="dev__img">
+                            <figcaption class="dev__caption">Nikhil Kumar Suman</figcaption>
+                        </figure>
+                        <div class="dev__name"><strong>Nikhil Kumar Suman</strong></div>
+                        <a href="mailto:nikhils@gmail.com" class="dev__profile">
+                            <i class="far fa-envelope dev__profile--icon"></i>
+                        </a>
+                    </div>
+
+                    <div class="col-1-of-4">
+                        <figure class="dev__shape">
+                            <img src="img/2k18/mentor/mentor11.jpg" alt="Pothukuchi Siddhartha" class="dev__img">
+                            <figcaption class="dev__caption">Pothukuchi Siddhartha</figcaption>
+                        </figure>
+                        <div class="dev__name"><strong>Pothukuchi Siddhartha</strong></div>
+                        <a href="mailto:pothukuchis@iitbhilai.ac.in" class="dev__profile">
+                            <i class="far fa-envelope dev__profile--icon"></i>
+                        </a>
+                    </div>
+
+                </div>
+            </div>
+
+            <div class="row">
+                <div class="dev">
+                  <div class="col-1-of-4">
+                      <figure class="dev__shape">
+                          <img src="img/2k18/mentor/mentor21.jpeg" alt="Prashant Kumar Meena" class="dev__img">
+                          <figcaption class="dev__caption">Prashant Kumar Meena</figcaption>
+                      </figure>
+                      <div class="dev__name"><strong>Prashant Kumar Meena</strong></div>
+                      <a href="mailto:prashantm@iitbhilai.ac.in" class="dev__profile">
+                          <i class="far fa-envelope dev__profile--icon"></i>
+                      </a>
+                  </div>
+
+                  <div class="col-1-of-4">
+                      <figure class="dev__shape">
+                          <img src="img/2k18/mentor/mentor12.jpg" alt="Pratik Sanjay Patil" class="dev__img">
+                          <figcaption class="dev__caption">Pratik Sanjay Patil</figcaption>
+                      </figure>
+                      <div class="dev__name"><strong>Pratik Sanjay Patil</strong></div>
+                      <a href="mailto:pratikp@iitbhilai.ac.in" class="dev__profile">
+                          <i class="far fa-envelope dev__profile--icon"></i>
+                      </a>
+                  </div>
+                    <div class="col-1-of-4">
+                        <figure class="dev__shape">
+                            <img src="img/2k18/mentor/mentor13.jpg" alt="Pritesh Roshan" class="dev__img">
+                            <figcaption class="dev__caption">Pritesh Roshan</figcaption>
+                        </figure>
+                        <div class="dev__name"><strong>Pritesh Roshan</strong></div>
+                        <a href="mailto:priteshp@iitbhilai.ac.in" class="dev__profile">
+                            <i class="far fa-envelope dev__profile--icon"></i>
+                        </a>
+                    </div>
+
+                    <div class="col-1-of-4">
+                        <figure class="dev__shape">
+                            <img src="img/2k18/mentor/mentor14.jpg" alt="Saksham Bhushan" class="dev__img">
+                            <figcaption class="dev__caption">Saksham Bhushan</figcaption>
+                        </figure>
+                        <div class="dev__name"><strong>Saksham Bhushan</strong></div>
+                        <a href="mailto:sakshamb@iitbhilai.ac.in" class="dev__profile">
+                            <i class="far fa-envelope dev__profile--icon"></i>
+                        </a>
+                    </div>
+
+
+                </div>
+            </div>
+
+            <div class="row">
+                <div class="dev">
+                  <div class="col-1-of-4">
+                      <figure class="dev__shape">
+                          <img src="img/2k18/mentor/mentor22.JPG" alt="Shiva kant Sonkar" class="dev__img">
+                          <figcaption class="dev__caption">Shiva kant Sonkar</figcaption>
+                      </figure>
+                      <div class="dev__name"><strong>Shiva kant Sonkar</strong></div>
+                      <a href="mailto:shivak@iitbhilai.ac.in" class="dev__profile">
+                          <i class="far fa-envelope dev__profile--icon"></i>
+                      </a>
+                  </div>
+
+                  <div class="col-1-of-4">
+                      <figure class="dev__shape">
+                          <img src="img/2k18/mentor/mentor17.jpg" alt="Tanish Gupta" class="dev__img">
+                          <figcaption class="dev__caption">Tanish Gupta</figcaption>
+                      </figure>
+                      <div class="dev__name"><strong>Tanish Gupta</strong></div>
+                      <a href="mailto:tanish@iitbhilai.ac.in" class="dev__profile">
+                          <i class="far fa-envelope dev__profile--icon"></i>
+                      </a>
+                  </div>
+
+                  <div class="col-1-of-4">
+                      <figure class="dev__shape">
+                          <img src="img/2k18/mentor/mentor18.jpg" alt="Thummala Milind Kesar" class="dev__img">
+                          <figcaption class="dev__caption">Thummala Milind Kesar</figcaption>
+                      </figure>
+                      <div class="dev__name"><strong>Thummala Milind Kesar</strong></div>
+                      <a href="mailto:thummalam@iitbhilai.ac.in" class="dev__profile">
+                          <i class="far fa-envelope dev__profile--icon"></i>
+                      </a>
+                  </div>
+
+                  <div class="col-1-of-4">
+                      <figure class="dev__shape">
+                          <img src="img/2k18/mentor/mentor19.jpg" alt="Utkarsh Gupta" class="dev__img">
+                          <figcaption class="dev__caption">Utkarsh Gupta</figcaption>
+                      </figure>
+                      <div class="dev__name"><strong>Utkarsh Gupta</strong></div>
+                      <a href="mailto:utkarsh@iitbhilai.ac.in" class="dev__profile">
+                          <i class="far fa-envelope dev__profile--icon"></i>
+                      </a>
+                  </div>
+
+
+                </div>
+            </div>
+        </section>
+
+        <div class="line-break"></div>
+
+        <section> 
+            <div class="container">
+                <h2 class="heading-secondary">Mentors-PG</h2>
+            </div>
+            <div class="row">
+                <div class="dev">
+                  <div class="col-1-of-4">
+                      <figure class="dev__shape">
+                          <img src="img/2k18/mentor/mentor23.png" alt="Ashish Vishwakarma" class="dev__img">
+                          <figcaption class="dev__caption">Ashish Vishwakarma</figcaption>
+                      </figure>
+                      <div class="dev__name"><strong>Ashish Vishwakarma</strong></div>
+                      <a href="mailto:ashishv@iitbhilai.ac.in" class="dev__profile">
+                          <i class="far fa-envelope dev__profile--icon"></i>
+                      </a>
+                  </div>
+
+                  <div class="col-1-of-4">
+                      <figure class="dev__shape">
+                          <img src="img/2k18/mentor/mentor24.jpg" alt="Debabrota Sadhukhan" class="dev__img">
+                          <figcaption class="dev__caption">Debabrota Sadhukhan</figcaption>
+                      </figure>
+                      <div class="dev__name"><strong>Debabrota Sadhukhan</strong></div>
+                      <a href="mailto:debabrotas@iitbhilai.ac.in" class="dev__profile">
+                          <i class="far fa-envelope dev__profile--icon"></i>
+                      </a>
+                  </div>
+                  <div class="col-1-of-4">
+                      <figure class="dev__shape">
+                          <img src="img/2k18/mentor/mentor15.jpg" alt="Shubham Giri" class="dev__img">
+                          <figcaption class="dev__caption">Shubham Giri</figcaption>
+                      </figure>
+                      <div class="dev__name"><strong>Shubham Giri</strong></div>
+                      <a href="mailto:shubhamg@iitbhilai.ac.in" class="dev__profile">
+                          <i class="far fa-envelope dev__profile--icon"></i>
+                      </a>
+                  </div>
+
+                  <div class="col-1-of-4">
+                      <figure class="dev__shape">
+                          <img src="img/2k18/mentor/mentor16.jpg" alt="Somesh Sharma" class="dev__img">
+                          <figcaption class="dev__caption">Somesh Sharma</figcaption>
+                      </figure>
+                      <div class="dev__name"><strong>Somesh Sharma</strong></div>
+                      <a href="mailto:someshs@iitbhilai.ac.in" class="dev__profile">
+                          <i class="far fa-envelope dev__profile--icon"></i>
+                      </a>
+                  </div>
+
+                </div>
+            </div>
+        </section>
+
+
+        <?php
+        require_once('footer.php');
+        ?>
+    </body>
+
+</html>
